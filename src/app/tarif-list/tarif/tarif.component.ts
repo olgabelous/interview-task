0b1989430c2d5cc50e1b55b8c6fa3c67e20488f2
Olga Belous <olga.shahray@gmail.com>
2025-01-20 19:59:56 +0100

    Add Currency pipe

--- src/app/tarif-list/tarif/tarif.component.ts
+++ src/app/tarif-list/tarif/tarif.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Tarif } from '../../models/tarif';
 import { MatButtonModule } from '@angular/material/button';
+import { CurrencyPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-tarif',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CurrencyPipe],
   templateUrl: './tarif.component.html',
   styleUrl: './tarif.component.scss'
 })
