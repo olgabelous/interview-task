574a3de4753442873d3a0a8da75be4be789052c0
Olga Belous <olga.shahray@gmail.com>
2025-01-21 16:11:26 +0100

    Add lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
-import { TariffDetailsComponent } from './tariff-list/tariff-details/tariff-details.component';
 import { TariffListComponent } from './tariff-list/tariff-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'tariffs', pathMatch: 'full' }, 
     { path: 'tariffs', component: TariffListComponent }, 
-    { path: 'tariffs/:tariffId', component: TariffDetailsComponent }, 
+    { 
+        path: 'tariffs/:tariffId', 
+        loadComponent: () =>
+        import('./tariff-list/tariff-details/tariff-details.component').then(
+          (m) => m.TariffDetailsComponent
+        ), 
+    }, 
 ];
