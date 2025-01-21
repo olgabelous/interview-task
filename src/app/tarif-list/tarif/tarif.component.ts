636a6f2cb9d1eac6a20b27a82a0654cbdf30d3f3
Olga Belous <olga.shahray@gmail.com>
2025-01-21 14:08:06 +0100

    Add desktop version

--- src/app/tarif-list/tarif/tarif.component.ts
+++ src/app/tarif-list/tarif/tarif.component.ts
@@ -3,11 +3,13 @@ import { Tarif } from '../../models/tarif';
 import { MatButtonModule } from '@angular/material/button';
 import { CurrencyPipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatListModule} from '@angular/material/list';
 
 
 @Component({
   selector: 'app-tarif',
-  imports: [MatButtonModule, CurrencyPipe, MatIconModule],
+  imports: [MatButtonModule, CurrencyPipe, MatIconModule, MatChipsModule, MatListModule],
   templateUrl: './tarif.component.html',
   styleUrl: './tarif.component.scss'
 })
